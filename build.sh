df16de7ea92ace27d51cc531ff49cbcbc39fbadc
Konstantinos Goutsos <kgoutsos@outlook.com>
2016-02-26 12:10:56 +0000

    updated openjdk deb urls

--- build.sh
+++ build.sh
@@ -10,48 +10,48 @@ DEVIVEHIVEVER="2.0.9"
 DEVICEHIVEADMINCPVER="2.0.2"
 
 if [ "$1" = "--noarm" ]; then
-  NOARM="1"
-  echo armhf is disabled
+    NOARM="1"
+    echo armhf is disabled
 fi
 
 function download() {
-  mkdir -p $CACHEDIR
-  (cd $CACHEDIR && wget -nc $1)
+    mkdir -p $CACHEDIR
+    (cd $CACHEDIR && wget -nc $1)
 }
 
 function unpack() {
-  (cd $CACHEDIR && tar xf $1)
+    (cd $CACHEDIR && tar xf $1)
 }
 
 function build_postgre() {
-  cp $CACHEDIR/postgresql-$POSTGREVER $CACHEDIR/postgresql-$POSTGREVER-$1 -rn
-  if [ ! -f $CACHEDIR/postgresql-$POSTGREVER-$1/config.status ]; then
-    (cd $CACHEDIR/postgresql-$POSTGREVER-$1 && ./configure --disable-float8-byval --without-openssl --without-pam --without-perl --without-python --without-readline --without-tcl --host=$2 --prefix=$BUILDDIR/psql-$1)
-  fi
-  (cd $CACHEDIR/postgresql-$POSTGREVER-$1 && make -j 8 && make install)
+    cp $CACHEDIR/postgresql-$POSTGREVER $CACHEDIR/postgresql-$POSTGREVER-$1 -rn
+    if [ ! -f $CACHEDIR/postgresql-$POSTGREVER-$1/config.status ]; then
+        (cd $CACHEDIR/postgresql-$POSTGREVER-$1 && ./configure --disable-float8-byval --without-openssl --without-pam --without-perl --without-python --without-readline --without-tcl --host=$2 --prefix=$BUILDDIR/psql-$1)
+    fi
+    (cd $CACHEDIR/postgresql-$POSTGREVER-$1 && make -j 8 && make install)
 }
 
 function mk_postgre() {
-  download https://ftp.postgresql.org/pub/source/v$POSTGREVER/postgresql-$POSTGREVER.tar.gz
-  unpack postgresql-$POSTGREVER.tar.gz
-  (cd $CACHEDIR/postgresql-$POSTGREVER && patch -Np0 -i $DIR/psql-disable-security.patch)
-  if [ -z $NOARM ]; then
-    build_postgre armv7l arm-linux-gnueabihf
-  fi
-  build_postgre x86_64 x86_64-linux-gnu
+    download https://ftp.postgresql.org/pub/source/v$POSTGREVER/postgresql-$POSTGREVER.tar.gz
+    unpack postgresql-$POSTGREVER.tar.gz
+    (cd $CACHEDIR/postgresql-$POSTGREVER && patch -Np0 -i $DIR/psql-disable-security.patch)
+    if [ -z $NOARM ]; then
+        build_postgre armv7l arm-linux-gnueabihf
+    fi
+    build_postgre x86_64 x86_64-linux-gnu
 }
 
 function mk_java() {
-  download http://http.us.debian.org/debian/pool/main/o/openjdk-8/openjdk-8-jre_8u72-b05-1_$1.deb
-  download http://http.us.debian.org/debian/pool/main/o/openjdk-8/openjdk-8-jre-jamvm_8u72-b05-1_$1.deb
-  download http://http.us.debian.org/debian/pool/main/o/openjdk-8/openjdk-8-jre-headless_8u72-b05-1_$1.deb
-  dpkg -x $CACHEDIR/openjdk-8-jre_8u72-b05-1_$1.deb $CACHEDIR/java-$2
-  dpkg -x $CACHEDIR/openjdk-8-jre-jamvm_8u72-b05-1_$1.deb $CACHEDIR/java-$2
-  dpkg -x $CACHEDIR/openjdk-8-jre-headless_8u72-b05-1_$1.deb $CACHEDIR/java-$2
-  cp $CACHEDIR/java-$2/etc/java-8-openjdk/* $CACHEDIR/java-$2/usr/lib/jvm/java-8-openjdk-$1/jre/lib/ -r --remove-destination
-  cp $CACHEDIR/java-$2/etc/java-8-openjdk/jvm-$1.cfg $CACHEDIR/java-$2/usr/lib/jvm/java-8-openjdk-$1/jre/lib/$3/jvm.cfg --remove-destination
-  mkdir -p $BUILDDIR/java-$2
-  cp $CACHEDIR/java-$2/usr/lib/jvm/java-8-openjdk-$1/jre/* $BUILDDIR/java-$2 -rn
+    download http://http.us.debian.org/debian/pool/main/o/openjdk-8/openjdk-8-jre_8u72-b05-6_$1.deb
+    download http://http.us.debian.org/debian/pool/main/o/openjdk-8/openjdk-8-jre-jamvm_8u72-b05-6_$1.deb
+    download http://http.us.debian.org/debian/pool/main/o/openjdk-8/openjdk-8-jre-headless_8u72-b05-6_$1.deb
+    dpkg -x $CACHEDIR/openjdk-8-jre_8u72-b05-6_$1.deb $CACHEDIR/java-$2
+    dpkg -x $CACHEDIR/openjdk-8-jre-jamvm_8u72-b05-6_$1.deb $CACHEDIR/java-$2
+    dpkg -x $CACHEDIR/openjdk-8-jre-headless_8u72-b05-6_$1.deb $CACHEDIR/java-$2
+    cp $CACHEDIR/java-$2/etc/java-8-openjdk/* $CACHEDIR/java-$2/usr/lib/jvm/java-8-openjdk-$1/jre/lib/ -r --remove-destination
+    cp $CACHEDIR/java-$2/etc/java-8-openjdk/jvm-$1.cfg $CACHEDIR/java-$2/usr/lib/jvm/java-8-openjdk-$1/jre/lib/$3/jvm.cfg --remove-destination
+    mkdir -p $BUILDDIR/java-$2
+    cp $CACHEDIR/java-$2/usr/lib/jvm/java-8-openjdk-$1/jre/* $BUILDDIR/java-$2 -rn
 }
 
 # Build PostreSQL
@@ -59,7 +59,7 @@ mk_postgre
 
 # Build Java
 if [ -z $NOARM ]; then
-  mk_java armhf armv7l arm
+    mk_java armhf armv7l arm
 fi
 mk_java amd64 x86_64 amd64
 
